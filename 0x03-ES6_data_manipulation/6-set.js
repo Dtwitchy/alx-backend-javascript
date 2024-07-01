4e2b081414980fc01c68081cda0d5b78149b472c
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2024-07-01 10:51:09 -0700

    fix return

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,5 +1,8 @@
-const setFromArray = (array) => (
-  new Set(array)
-);
-
-export default setFromArray;
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
