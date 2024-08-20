9d2fa5611228160fba032580229b05d42b870bc1
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2024-08-20 06:37:36 -0700

    okay

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Destiny Aghafekokhian <https://github.com/Dtwitchy>
+ */
 const displayMessage = (msg) => {
   console.log(msg);
 };
